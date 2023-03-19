922a8ce85950485c35617f967d4d146a1c499800
ScillFury <127161247+ScillFury@users.noreply.github.com>
2023-03-20 03:54:02 +0900

    Update gui.py

--- gui.py
+++ gui.py
@@ -2,5 +2,5 @@ from flaskwebgui import FlaskUI
 from whisper import stt
 from main import app
 
-
+# Created by ScillFury(scillfury@gmail.com)
 FlaskUI(server='flask', app= app, fullscreen=True, port=1234,).run()
